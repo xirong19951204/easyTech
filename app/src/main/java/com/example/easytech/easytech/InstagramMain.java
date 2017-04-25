3a1a59d8df9386a2acc65b9bf320e3f44acbdf8d
LIU <lliujingxing@gmail.com>
2017-04-24 21:25:33 -0700

    Basic Structure test

--- app/src/main/java/com/example/easytech/easytech/InstagramMain.java
+++ app/src/main/java/com/example/easytech/easytech/InstagramMain.java
@@ -5,29 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class InstagramMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_instagram_main);
     }
-
-    public void fb(View v){
+    public void ins1(View v){
         Intent gotoFB = new Intent();
         gotoFB.setClass(this, FacebookMain.class);
         startActivity(gotoFB);
     }
-    public void twi(View v){
-        Intent gototwi = new Intent();
-        gototwi.setClass(this, TwitterMain.class);
-        startActivity(gototwi);
+    public void ins2(View v){
+        Intent gotoFB = new Intent();
+        gotoFB.setClass(this, FacebookMain.class);
+        startActivity(gotoFB);
     }
-    public void ins(View v){
-        Intent gotoins = new Intent();
-        gotoins.setClass(this, InstagramMain.class);
-        startActivity(gotoins);
+    public void ins3(View v){
+        Intent gotoFB = new Intent();
+        gotoFB.setClass(this, FacebookMain.class);
+        startActivity(gotoFB);
     }
     public void home(View v) {
         Intent gotoHome = new Intent();
--- app/src/main/java/com/example/easytech/easytech/TwitterMain.java
+++ app/src/main/java/com/example/easytech/easytech/TwitterMain.java
@@ -5,29 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class MainActivity extends AppCompatActivity {
+public class TwitterMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_twitter_main);
     }
-
-    public void fb(View v){
+    public void twi1(View v){
         Intent gotoFB = new Intent();
         gotoFB.setClass(this, FacebookMain.class);
         startActivity(gotoFB);
     }
-    public void twi(View v){
-        Intent gototwi = new Intent();
-        gototwi.setClass(this, TwitterMain.class);
-        startActivity(gototwi);
+    public void twi2(View v){
+        Intent gotoFB = new Intent();
+        gotoFB.setClass(this, FacebookMain.class);
+        startActivity(gotoFB);
     }
-    public void ins(View v){
-        Intent gotoins = new Intent();
-        gotoins.setClass(this, InstagramMain.class);
-        startActivity(gotoins);
+    public void twi3(View v){
+        Intent gotoFB = new Intent();
+        gotoFB.setClass(this, FacebookMain.class);
+        startActivity(gotoFB);
     }
     public void home(View v) {
         Intent gotoHome = new Intent();
